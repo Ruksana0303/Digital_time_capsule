893a0109d00e1774ab86e81e843b491e7f005115
Ruksana Shaik <shaikruksana0303@gmail.com>
2026-03-02 20:47:58 +0530

    time capsule

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 api.interceptors.request.use((config) => {
